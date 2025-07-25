924af1353296d95a787b7ef9316ce8c4aa6c4ba8
Mahansa <hansagamers11@gmail.com>
2025-07-25 15:24:34 +0700

    Add files via upload

---
+++ if else/If else.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+ int temp= 100;
+  if(temp >= 102){
+   std::cout << "You have a fever. 🤒";
+  }else{
+   std::cout << "You do not have a fever. 🕺";
+  }
+  
+}
